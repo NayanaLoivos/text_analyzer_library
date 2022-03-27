546cd1cb6d06f85f17361f89e246752491513f66
Nayana Loivos <loivosdev@gmail.com>
2022-03-26 21:52:24 -0300

    Unindo array de links e status num único objeto

--- http-validacao.js
+++ http-validacao.js
@@ -2,21 +2,30 @@ import fetch from 'node-fetch';
 
 async function checkStatus(arrayURLs) {
     //promises async await
-    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
-        const res = await fetch(url)
-        return res.status;
+    const arrayStatus = await Promise
+        .all(arrayURLs
+            .map(async url => {
+                const res = await fetch(url)
+                return res.status;
     }))
     return arrayStatus;    
 }
 
 function gerateURLs(arrayLinks) {    
-   return arrayLinks.map(objectLink => Object.values(objectLink).join());
+    return arrayLinks
+        .map(objectLink => Object
+            .values(objectLink).join());
 }
 
 export default async function validateURLs(arrayLinks) {
     const links = gerateURLs(arrayLinks);
     const linksStatus = await checkStatus(links);
-    return linksStatus;
+    
+    const resultados = arrayLinks.map((object, index) => ({
+        ...object, 
+        status: linksStatus[index]
+    }))
+    return resultados;
 }
 
 
